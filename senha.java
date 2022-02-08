25f9230ea48e4a47ecadb4d9559fbc825af72742
Vanessa Santos <98504873+VanessaSotnas@users.noreply.github.com>
2022-02-07 21:41:46 -0300

    Update senha.java

--- senha.java
+++ senha.java
@@ -6,14 +6,13 @@ public class senha {
 
 	public static void main(String[] args) {
 		
-	//Escreva um programa que verifique a validade de uma senha fornecida pelo usu·rio.
-	//A	senha v·lida È o n˙mero	1234. Se a senha for valida, deve aparecer ACESSO PERMITIDO, se n„o, ACESSO NEGADO.
+	//Escreva um programa que verifique a validade de uma senha fornecida pelo usu√°rio.
+	//A senha v√°lida √© o n√∫mero 1234. Se a senha for valida, deve aparecer ACESSO PERMITIDO, se n√£o, ACESSO NEGADO.
 		
 	Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite a senha: ");
 		int senha = sc.nextInt();
-		
 		
 		while (senha != 1234) {
 		System.out.println("ACESSO NEGADO.");
@@ -29,4 +28,4 @@ public class senha {
 	sc.close();
 	}
 
-}
+  }
